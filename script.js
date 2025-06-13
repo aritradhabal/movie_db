cb1f74dd641267bf61543c3bdd1d05e83e547cc7
Aritra Dhabal <63856734+aritradhabal@users.noreply.github.com>
2025-06-13 19:29:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,28 @@ const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=`;
 
 
 
-
-
-
 async function fetchData(userSearch){
     try
     {
         const response = await fetch(`${API_URL}${userSearch}`);
         const movieData = await response.json();
         
+
+        const section = document.querySelector('.all_cards');
+        section.innerHTML = '';
         if(movieData.Response=="True"){
             displayMovies(movieData.Search);
-            // console.log(movieData.Search[].Poster);
+            
         }
         else{
-            return;
+            if(userSearch.length>2){
+                const notFound = document.createElement('div');
+            notFound.className="notFound";
+            notFound.textContent=`🤦 No movies available with your keyword "${userSearch}"`;
+            section.appendChild(notFound);
+            }
+            
+                
         }
 
     }
@@ -33,7 +40,6 @@ async function fetchData(userSearch){
 function displayMovies(searches){
 
     const section = document.querySelector('.all_cards');
-
     section.innerHTML = '';
 
 
@@ -47,6 +53,7 @@ function displayMovies(searches){
         cards_items1.className="cards_items cards_items1";
         
         const img = document.createElement('img');
+
         img.src=`${searches[i].Poster}`;
         cards_items1.appendChild(img);
         div.appendChild(cards_items1);
@@ -78,59 +85,24 @@ function displayMovies(searches){
 
 
 
-
     }
-
-
-    // const section = document.querySelector('.all_cards')
-    // const div = document.createElement('div');
-    // div.className = "cards";
-    // section.appendChild(div);
-
-
-    // const cards_items1 = document.createElement('div');
-    // cards_items1.className="cards_items cards_items1";
-    
-    // const img = document.createElement('img');
-    // img.src=`${search[0].Poster}`;
-    // cards_items1.appendChild(img);
-    // div.appendChild(cards_items1);
-
-
-    // const cards_items2 = document.createElement('div');
-    // cards_items2.className="cards_items cards_items2";
-    // const Title = document.createElement('p');
-    // Title.textContent=`${search[0].Title}`;
-    // cards_items2.appendChild(Title);
-    // div.appendChild(cards_items2);
-
-
-    // const cards_items3 = document.createElement('div');
-    // cards_items3.className="cards_items cards_items3";
-    // const Year = document.createElement('p');
-    // Year.textContent=`${search[0].Year}`;
-    // cards_items3.appendChild(Year);
-    // div.appendChild(cards_items3);
-    // div.appendChild(cards_items3);
-
-    // const cards_items4 = document.createElement('div');
-    // cards_items4.className="cards_items cards_items4";
-    // const button = document.createElement('button');
-    // button.textContent="Add to Favorites";
-    // cards_items4.appendChild(button);
-    // div.appendChild(cards_items4);
-
-
-
-    
-
-
-    
     
 
 }
 
 input.addEventListener('input', ()=>{
+    const section = document.querySelector('.all_cards');
+    section.innerHTML = '';
     const userSearch = input.value.trim();
-    fetchData(userSearch);
+    if(userSearch.length<3 && userSearch.length>0){
+        const tooShort = document.createElement('div');
+        tooShort.className="too_short";
+        tooShort.textContent="⌨️ Please enter more than 2 characters to search!";
+        section.appendChild(tooShort);
+    }
+    else{
+fetchData(userSearch);
+    }
+    
 })
+
